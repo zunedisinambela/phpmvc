c9e11563d074173516d5db949c54bf6d4cf7d466
zunedisinambela <zunedisinambela@gmail.com>
2021-10-26 11:39:22 +0700

    Delete Data

--- app/views/mahasiswa/index.php
+++ app/views/mahasiswa/index.php
@@ -11,9 +11,10 @@
             <h3>Daftar Mahasiswa</h3>
             <ul class="list-group">
                 <?php foreach ($data['mhs'] as $mhs) : ?>
-                <li class="list-group-item  d-flex justify-content-between align-items-center">
+                <li class="list-group-item">
                     <?= $mhs['nama']; ?>
-                    <a href="<?= BASEURL; ?>/mahasiswa/detail/<?= $mhs['id']; ?>" class="badge badge-primary">detail</a>
+                    <a href="<?= BASEURL; ?>/mahasiswa/hapus/<?= $mhs['id']; ?>" class="badge badge-danger float-right ml-1" onclick="return confirm">hapus</a>
+                    <a href="<?= BASEURL; ?>/mahasiswa/detail/<?= $mhs['id']; ?>" class="badge badge-primary float-right ml-1">detail</a>
                 </li>
                 <?php endforeach; ?>
             </ul>
